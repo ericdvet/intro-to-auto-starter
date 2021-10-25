6260a6d6acd096cbfcb58bd043a6c97ab65c24d8
Eric Vetha <evetha@ucsc.edu>
2021-10-25 01:32:00 -0700

    did stuff in wrong folder

--- lab3/scripts/safety_node.py
+++ lab3/scripts/safety_node.py
@@ -67,4 +67,4 @@ class Safety(object):
 if __name__ == '__main__':
     rospy.init_node('safety_node')
     safety_node = Safety()
-    rospy.spin() 
+    rospy.spin()
---
+++ lab4-launch-sample/scripts/control_node.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+import rospy
+
+if __name__ == '__main__':
+    rospy.init_node('control_node')
+    #perception_node = Perception()
+    rospy.spin()
