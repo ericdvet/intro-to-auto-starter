2023ed20d5c4fc681318f1ee0a47c49a826f3b77
Eric Vetha <evetha@ucsc.edu>
2021-11-18 04:19:45 -0800

    used numpy to make loops more efficient

--- lab5/scripts/gap_finder.py
+++ lab5/scripts/gap_finder.py
@@ -8,7 +8,7 @@ import numpy as np
 class GapFinder:
     def __init__(self):
         rospy.init_node('gap_finder', anonymous=True)
-        self.error_pub = rospy.Publisher('/gap_error', SteeringInput, queue_size=10)
+        self.error_pub = rospy.Publisher('/gap_finder', SteeringInput, queue_size=10)
         rospy.Subscriber("/scan", LaserScan, self.scan_callback)
     
     def get_range(self, data, theta):
@@ -16,32 +16,16 @@ class GapFinder:
 
     def scan_callback(self, data):
 
-        safetyRanges = list(data.ranges)
-        closestDist = data.ranges[0]
-        closesti = 0
-        for (i, dist) in enumerate(safetyRanges):
-            if dist > data.range_min and dist < data.range_max and dist != np.nan and dist != np.inf:
-                if dist < closestDist:
-                    closestDist = dist
-                    closesti = i
-        
-        bubbleSize = 10
-        for i in range(closesti - int(closestDist*bubbleSize), closesti + int(closestDist*bubbleSize) ):
-            if i > 0 and i < len(safetyRanges):
-                safetyRanges[i] = 0
-        
-        farthestDist = data.ranges[0]
-        farthesti = 0
-        farthestAngle = data.angle_min
-        angle = data.angle_min
-        for (i, dist) in enumerate(safetyRanges):
-            if dist > data.range_min and dist < data.range_max and dist != np.nan and dist != np.inf:
-                if dist > farthestDist:
-                    farthestDist = dist
-                    farthesti = i
-                    farthestAngle = angle
-            angle += data.angle_increment
-        print(farthestAngle, farthestDist, farthesti)
+        safeRanges = np.array(data.ranges[135:675])
+
+        bubble = 140
+        closesti = np.argmin(safeRanges)
+        closestDist = np.amin(safeRanges)
+        for i in range(closesti - int(bubble * closestDist), closesti + int(bubble * closestDist)):
+            if i > 0 and i < len(safeRanges):
+                safeRanges[i] = 0
+        farthesti = np.argmax(safeRanges)
+        farthestAngle = farthesti * data.angle_increment - (math.pi / 2.0)
 
         self.error_pub.publish(farthestAngle)
 
