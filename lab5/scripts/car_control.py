20f15dbf0a88bcc192e42a5dae5d9ef4fadd86f1
Eric Vetha <evetha@ucsc.edu>
2021-11-17 21:05:59 -0800

    new implementation of gap_finger.py

--- lab5/scripts/car_control.py
+++ lab5/scripts/car_control.py
@@ -5,6 +5,9 @@ from ackermann_msgs.msg import AckermannDriveStamped
 from geometry_msgs.msg import PoseStamped
 import math
 
+ERROR_SCALE = 1
+MAX_STEERING_ANGLE = 4.18
+
 class CarControl:
     def __init__(self):
         rospy.init_node("car_control", anonymous=False)
@@ -12,11 +15,21 @@ class CarControl:
         self.pose_pub = rospy.Publisher('/pose', PoseStamped, queue_size=10)
         rospy.Subscriber('/gap_error', SteeringInput, self.run_car)
         rospy.on_shutdown(self.on_shutdown)
+        self.kP = 1
 
     def run_car(self, steering_input):
+        SCALED_ERROR = steering_input.steering_angle * ERROR_SCALE
+        steering_angle = self.kP * SCALED_ERROR
+
+        if (steering_angle > MAX_STEERING_ANGLE):
+            steering_angle = MAX_STEERING_ANGLE
+        if (steering_angle < (-1 * MAX_STEERING_ANGLE)):
+            steering_angle = -1 * MAX_STEERING_ANGLE
+
         drive = AckermannDriveStamped()
-        drive.drive.steering_angle = steering_input.steering_angle
+        drive.drive.steering_angle = steering_angle
         drive.drive.speed = 2
+
         self.drive_pub.publish(drive)
         pass
 
--- lab5/scripts/gap_finder.py
+++ lab5/scripts/gap_finder.py
@@ -15,16 +15,35 @@ class GapFinder:
         return data.ranges[int(theta * (len(data.ranges) / 270.0))]
 
     def scan_callback(self, data):
-        farthestDist = data.ranges[0]
-        farthestAngle = data.angle_min
-        anglei = 0
-        for (i, dist) in enumerate(data.ranges):
+
+        safetyRanges = list(data.ranges)
+        closestDist = data.ranges[0]
+        closesti = 0
+        for (i, dist) in enumerate(safetyRanges):
             if dist > data.range_min and dist < data.range_max and dist != np.nan and dist != np.inf:
-                if (dist > farthestDist):
+                if dist < closestDist:
+                    closestDist = dist
+                    closesti = i
+        
+        bubbleSize = 10
+        for i in range(closesti - int(closestDist*bubbleSize), closesti + int(closestDist*bubbleSize) ):
+            if i > 0 and i < len(safetyRanges):
+                safetyRanges[i] = 0
+        
+        farthestDist = data.ranges[0]
+        farthesti = 0
+        farthestAngle = data.angle_min
+        angle = data.angle_min
+        for (i, dist) in enumerate(safetyRanges):
+            if dist > data.range_min and dist < data.range_max and dist != np.nan and dist != np.inf:
+                if dist > farthestDist:
                     farthestDist = dist
-                    farthestAngle = anglei
-            anglei += data.angle_increment
-        self.error_pub.publish(farthestAngle - (math.pi / 4.0) - (math.pi / 2.0))
+                    farthesti = i
+                    farthestAngle = angle
+            angle += data.angle_increment
+        print(farthestAngle, farthestDist, farthesti)
+
+        self.error_pub.publish(farthestAngle)
 
 if __name__ == "__main__":
     GapFinder()
